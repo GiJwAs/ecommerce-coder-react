52edc933a6c9fd4206b97c5f897cd0de2bbf9a97
Daniel Di Leva <danieldileva@hotmail.com>
2022-03-15 18:22:38 -0300

    creacion proyecto React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn 
         </a>
+        <Button >REACT</Button>
       </header>
     </div>
   );
